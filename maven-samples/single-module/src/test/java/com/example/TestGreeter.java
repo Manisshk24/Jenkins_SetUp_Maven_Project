e106ca7e6a9a70b744f74da2119f29a71394fe2b
Kumar, Manish <manisshk24@gmail.com>
2020-04-19 16:45:14 +0530

    Commenting the lines TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -24,14 +24,14 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
 
-  @Test
+/*  @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
+  } */
 
-    @Test
+  @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
 
